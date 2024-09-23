9ce790912d5605559eb8f8464f4a9fb990f09018
diotrier <ramadhantrie9@gmail.com>
2024-09-23 22:25:24 +0700

    fourth commit

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
@@ -56,6 +57,8 @@ app.post('/logout', (req, res) => {
     // Client-side can handle removing token
     res.status(200).send('Logged out');
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
